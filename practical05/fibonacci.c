0d298a816f9df4d74366d19331e9c651fab2de92
John Ibrahim <john@Johns-MacBook-Air.local>
2023-10-27 12:39:41 +0100

    Add practical5

---
+++ practical05/fibonacci.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+//Function declaration/prototype
+//Input arguments: FN-1 and Fn-2
+//On exit: Fn and Fn-1
+
+void fibonacci(int *a, int *b);
+
+int main(){
+
+
+  int n, i;
+  int f0=0, f1=1;
+
+  printf("Enter a positive integer\n");
+  scanf("%d", &n);
+
+  if(n<1){
+    printf("The number not positive.\n");
+    exit(1);
+  }
+
+
+  printf("The fibonacci sequence is: \n");
+  printf("%d, %d,", f0, f1);
+ 
+ //Calculating the fibonacci sequence from 2
+ for(i=2; i<=n; i++){
+   fibonacci(&f1, &f0);
+   printf("%d,", f1);
+
+   if((i+1)%10 == 0) printf("\n");
+ }
+  
+  return 0;
+}
+
+void fibonacci(int *a, int *b){
+  int next;
+  //*a=fn-1 *b=fn-2 next=fn
+
+  next=*a+*b;
+  //*a=fn *b=fn-1
+  *b=*a;
+  *a=next;
+
+
+
+}
